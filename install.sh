127f896e87d54c03f84a976c1ceccf879cbb4156
Ahmad Tanwir <mail@ata.web.id>
2016-06-28 07:49:47 +0700

    Change repo to over ssh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export VIM_REPO="https://github.com/KMK-ONLINE/vim-config.git"
+export VIM_REPO="git@github.com:KMK-ONLINE/vim-config.git"
 export VIM_INSTALL_DIR="${HOME}/.vim-config"
 export VIM_DIR="${HOME}/.vim"
 
